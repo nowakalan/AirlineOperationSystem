25b02fdfabd81ff711ce5eb50b153195882a1819
alannowak <nowak.alan@gmail.com>
2023-05-13 09:37:25 +0200

    dzialajacy login i rejestracja + podpiete menu

--- src/main/java/AirlanesTicketsOperationSystem/Main.java
+++ src/main/java/AirlanesTicketsOperationSystem/Main.java
@@ -1,9 +1,8 @@
 package AirlanesTicketsOperationSystem;
 
-import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) throws IOException, ClassNotFoundException {
+    public static void main(String[] args) {
         Menu menu = new Menu();
         UserLoginSystem loginSystem = new UserLoginSystem();
 
